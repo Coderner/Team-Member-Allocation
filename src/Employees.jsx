72e1de886a4676a92e8457188be90f983f866535
Coderner <shreyashukla680@gmail.com>
2023-03-31 19:13:03 +0530

    Created select team drop down menu

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -3,6 +3,8 @@ import femaleProfile from "./Images/femaleProfile.jpg";
 import maleProfile from "./Images/maleProfile.jpg";
 
 const Employees = () =>{
+
+   const [selectedTeam,setTeam] = useState("TeamB");
    const [employees,setEmployees] = useState([
     {
         id:1,
@@ -89,8 +91,25 @@ const Employees = () =>{
        teamName:"Team D"
      }
    ])
+
+   function handleTeamSelectionChange(event){
+     setTeam(event.target.value);
+   } 
+
    return(
        <main className="container">
+
+          <div class="row justify-content-center mt-3 mb-3">
+            <div class="col-6">
+              <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
+                <option value="TeamA">Team A</option>
+                <option value="TeamB">Team B</option>
+                <option value="TeamC">Team C</option>
+                <option value="TeamD">Team D</option>
+              </select>
+            </div>
+          </div>
+
           <div class="row justify-content-center mt-3 mb-3">
             <div class="col-8">
               <div class="card-collection">
